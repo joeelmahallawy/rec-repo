8169a0695c167e3e84f6a95ccc24bbb4775ea1ac
RossEnriquez <enriquez.ross091@gmail.com>
2021-11-20 11:57:31 -0500

    first commit

---
+++ pages/index.tsx
@@ -0,0 +1,26 @@
+import {
+  Box,
+  Button,
+  Center,
+  Flex,
+  Heading,
+  Icon,
+  IconButton,
+  Image,
+  Progress,
+  Text,
+  Wrap,
+  WrapItem,
+ } from "@chakra-ui/react";
+ import React, { useEffect, useState } from "react";
+ 
+const IndexPage = () => {
+  return(
+    <>
+    <Text>HI</Text>
+    </>
+  );
+
+};
+
+export default IndexPage;

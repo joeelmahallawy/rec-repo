44efde2b336d3f9dfbed1fdeb8832bc882b49f43
joeelmahallawy <youssef.elmahallawy01@gmail.com>
2021-11-20 13:18:40 -0500

    added db

--- pages/index.tsx
+++ pages/index.tsx
@@ -11,37 +11,40 @@ import {
   Text,
   Select,
   Input,
- } from "@chakra-ui/react";
- import React, { useEffect, useState } from "react";
+} from "@chakra-ui/react";
+import React, { useEffect, useState } from "react";
 
 const IndexPage = () => {
-  return(
+  return (
     <>
-    <Center bg="dodgerblue" h="275px">
-      <Text color="white" fontSize={20}>Indigenous Translation</Text>
-    </Center>
-    <Flex justifyContent="space-around" m="50px">
-      <Center w="250px">
-        <Text>English</Text>
+      <Center bg="dodgerblue" h="275px">
+        <Text color="white" fontSize={20}>
+          Indigenous Translation
+        </Text>
       </Center>
-      <Select w="250px">
-        <option value="Ind 1">Indigenous 1</option>
-        <option value="Ind 2">Indigenous 2</option>
-        <option value="Ind 3">Indigenous 3</option>
-      </Select>
-    </Flex>
-    <Flex justifyContent="space-around" m="50px">
-      <Input placeholder="English word here..." w="250px"></Input>
-      <Input placeholder="Indigenous word here..." isReadOnly={true} w="250px"></Input>
-    </Flex>
-    <Center>
-      <Button>Translate!</Button>
-    </Center>
-    
-    
+      <Flex justifyContent="space-around" m="50px">
+        <Center w="250px">
+          <Text>English</Text>
+        </Center>
+        <Select w="250px">
+          <option value="Ind 1">Indigenous 1</option>
+          <option value="Ind 2">Indigenous 2</option>
+          <option value="Ind 3">Indigenous 3</option>
+        </Select>
+      </Flex>
+      <Flex justifyContent="space-around" m="50px">
+        <Input placeholder="English word here..." w="250px"></Input>
+        <Input
+          placeholder="Indigenous word here..."
+          isReadOnly={true}
+          w="250px"
+        ></Input>
+      </Flex>
+      <Center>
+        <Button>Translate!</Button>
+      </Center>
     </>
   );
-
 };
 
 export default IndexPage;

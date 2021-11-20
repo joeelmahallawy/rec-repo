2ccd46d09749b7b3a22900e5d2554c2d46b586d4
RossEnriquez <enriquez.ross091@gmail.com>
2021-11-20 12:49:36 -0500

    added basic front end

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,15 +9,36 @@ import {
   Image,
   Progress,
   Text,
-  Wrap,
-  WrapItem,
+  Select,
+  Input,
  } from "@chakra-ui/react";
  import React, { useEffect, useState } from "react";
- 
+
 const IndexPage = () => {
   return(
     <>
-    <Text>HI</Text>
+    <Center bg="dodgerblue" h="275px">
+      <Text color="white" fontSize={20}>Indigenous Translation</Text>
+    </Center>
+    <Flex justifyContent="space-around" m="50px">
+      <Center w="250px">
+        <Text>English</Text>
+      </Center>
+      <Select w="250px">
+        <option value="Ind 1">Indigenous 1</option>
+        <option value="Ind 2">Indigenous 2</option>
+        <option value="Ind 3">Indigenous 3</option>
+      </Select>
+    </Flex>
+    <Flex justifyContent="space-around" m="50px">
+      <Input placeholder="English word here..." w="250px"></Input>
+      <Input placeholder="Indigenous word here..." isReadOnly={true} w="250px"></Input>
+    </Flex>
+    <Center>
+      <Button>Translate!</Button>
+    </Center>
+    
+    
     </>
   );
 
